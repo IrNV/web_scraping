1891aba16fe505b6f4db0553f27398a58ad41edc
Valera <NsT.valerka@ukr.net>
2018-03-30 22:45:29 +0300

    Add exceptions

--- first scraper.py
+++ first scraper.py
@@ -1,6 +1,26 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj)
+
+def get_title(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print("We can't take a page")
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        print("We can't find a tag")
+        return None
+
+    return title
+
+title = get_title("http://www.pythonscraping.com/exercises/exercise1.html")
+if title is None:
+    print("We have a problem")
+else:
+    print(title)
