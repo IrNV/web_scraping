4c1baa0ee0977a2592032ebdedabbae7cff23186
Valera <NsT.valerka@ukr.net>
2018-04-03 23:51:08 +0300

    find several tags

--- projects from book/BeautifulSoup_and_css.py
+++ projects from book/BeautifulSoup_and_css.py
@@ -5,5 +5,10 @@ html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html.read(), "html.parser")
 
 nameList = bsObj.findAll("span", {"class": "green"})
+headers = bsObj.findAll({"h1", "h2", "h3"})  # {} group of tags which we want to find
+
+for header in headers:
+    print(header.get_text())
+print("\n")
 for name in nameList:
     print(name.get_text())
