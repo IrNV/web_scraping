1c920c86a10774b7f504344f61d391025b890889
Valera <NsT.valerka@ukr.net>
2018-04-07 23:53:59 +0300

    Random links

--- projects from book/wikipedia.py
+++ projects from book/wikipedia.py
@@ -1,12 +1,20 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj = BeautifulSoup(html, "html.parser")
+random.seed(datetime.datetime.now())
 
-# Собераем все ссылки, которые ведут на статьи, а не на обсуждения или интсрументарий
-for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a",
-                                                             href=re.compile("^(/wiki/)((?!:).)*$")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+
+def get_links(article_url):
+    html = urlopen("http://en.wikipedia.org" + article_url)
+    bsObj = BeautifulSoup(html, "html.parser")
+    return bsObj.find("div", {"id": "bodyContent"}).findAll("a",
+                                                            href=re.compile("^(/wiki/)((?!:).)*$"))
+
+links = get_links("/wiki/Kevin_Bacon")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = get_links(newArticle)
