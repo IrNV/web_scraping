2d2c741fac3fc6e5379e09d4c5374128e90cb984
Valera <NsT.valerka@ukr.net>
2018-04-07 23:46:49 +0300

    check links

--- projects from book/wikipedia.py
+++ projects from book/wikipedia.py
@@ -1,9 +1,12 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
-# Собераем все ссылки на странице Кевина Бейкона
 html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
 bsObj = BeautifulSoup(html, "html.parser")
-for link in bsObj.findAll("a"):
+
+# Собераем все ссылки, которые ведут на статьи, а не на обсуждения или интсрументарий
+for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a",
+                                                             href=re.compile("^(/wiki/)((?!:).)*$")):
     if 'href' in link.attrs:
         print(link.attrs['href'])
