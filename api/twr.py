1b921c3eb8215a9074c74ceae8f98c31440513e3
Valera <NsT.valerka@ukr.net>
2018-04-09 23:48:50 +0300

    limit for twits

--- api/twr.py
+++ api/twr.py
@@ -1,6 +1,7 @@
 from twitter import *
-t = Twitter(auth=OAuth(<Access Token>,<Access Token Secret>,
-<Consumer Key>,<Consumer Secret>))
+t = Twitter(auth=OAuth("Access Token", "Access Token Secret",
+                       "Consumer Key", "Consumer Secret"))
 
-pythonTweets = t.search.tweets(q = "#python")
-print(pythonTweets)
+# Узнаем последние 5 твитов в ленте @montypython
+pythonStatuses = t.statuses.user_timeline(screen_name="montypython", count=5)
+print(pythonStatuses)
