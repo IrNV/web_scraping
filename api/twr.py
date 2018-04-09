f83868810df20876b5fa7a2f0bf9db2382a49940
Valera <NsT.valerka@ukr.net>
2018-04-10 00:00:19 +0300

    get first twit

--- api/twr.py
+++ api/twr.py
@@ -3,5 +3,9 @@ t = Twitter(auth=OAuth("Access Token", "Access Token Secret",
                        "Consumer Key", "Consumer Secret"))
 
 # Узнаем последние 5 твитов в ленте @montypython
-pythonStatuses = t.statuses.user_timeline(screen_name="montypython", count=5)
-print(pythonStatuses)
+# pythonStatuses = t.statuses.user_timeline(screen_name="montypython", count=5)
+# print(pythonStatuses)
+
+# Получаем первую запись и информацию о пользователя из наших твитов
+x = t.statuses.home_timeline()
+print(x[0]['user'])
