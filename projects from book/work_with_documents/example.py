12816d1757edacb8c8c5ce09e09cbf44ec090874
Valera <NsT.valerka@ukr.net>
2018-04-16 23:47:21 +0300

    utf8

--- projects from book/work_with_documents/example.py
+++ projects from book/work_with_documents/example.py
@@ -1,4 +1,13 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1.txt")
-print(textPage.read().decode("utf8"))
+# textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1.txt")
+# print(textPage.read().decode("utf8"))
+
+# Пример перевода текста в кодировку utf8
+html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
+bsObj = BeautifulSoup(html, "html.parser")
+content = bsObj.find("div", {"id": "mw-content-text"}).get_text()
+content = bytes(content, "UTF-8")
+content = content.decode("UTF-8")
+print(content)
