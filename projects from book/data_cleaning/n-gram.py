10efafb644c83ed79173129b7cd7870dbbc4f666
Valera <NsT.valerka@ukr.net>
2018-04-18 00:24:44 +0300

    cleaning text

--- projects from book/data_cleaning/n-gram.py
+++ projects from book/data_cleaning/n-gram.py
@@ -1,12 +1,33 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import string
+
+
+def clean_input(text):
+    """
+    Очистка данных
+    """
+    text = re.sub('\n+', " ", text)  # Замена символов переноса строки на пробел
+    text = re.sub('\[[0-9]*\]', "", text)  # Замена ссылок на пустую строку
+    text = re.sub(' +', " ", text)  # Замена нескольких пробелов на один
+    text = bytes(text, "UTF-8")
+    text = text.decode("ascii", "ignore")
+    clean_text = []
+    text = text.split(' ')
+    for item in text:
+        item = item.strip(string.punctuation)  # Удаляем знаки препинания
+        # Берем слова длинна которых больше 1 или это слова "a" и "i"
+        if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+            clean_text.append(item)
+    return clean_text
 
 
 def ngrams(text, n):
-    text = text.split(' ')
+    input = clean_input(text)
     output = []
-    for i in range(len(text) - n + 1):
-        output.append(text[i:i + n])
+    for i in range(len(input) - n + 1):
+        output.append(input[i:i + n])
     return output
 
 html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
