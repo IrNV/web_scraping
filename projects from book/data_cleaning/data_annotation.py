c7aa48b8e26491e87eaf69ac3754ec2be1c72a2a
Valera <NsT.valerka@ukr.net>
2018-04-20 23:37:13 +0300

    check that ngrams have unique words

--- projects from book/data_cleaning/data_annotation.py
+++ projects from book/data_cleaning/data_annotation.py
@@ -5,6 +5,31 @@ import string
 import operator
 
 
+def is_common(ngram):
+    """
+    Проверка входит ли n-грамма в самые распространенные слова,
+    для того что бы убрать n-граммы, по типу "to the", "to be", "in the"...
+    """
+    common_words = ["the", "be", "and", "of", "a", "in", "to", "have", "it",
+                    "i", "that", "for", "you", "he", "with", "on", "do", "say", "this",
+                    "they", "is", "an", "at", "but", "we", "his", "from", "that", "not",
+                    "by", "she", "or", "as", "what", "go", "their","can", "who", "get",
+                    "if", "would", "her", "all", "my", "make", "about", "know", "will",
+                    "as", "up", "one", "time", "has", "been", "there", "year", "so",
+                    "think", "when", "which", "them", "some", "me", "people", "take",
+                    "out", "into", "just", "see", "him", "your", "come", "could", "now",
+                    "than", "like", "other", "how", "then", "its", "our", "two", "more",
+                    "these", "want", "way", "look", "first", "also", "new", "because",
+                    "day", "more", "use", "no", "man", "find", "here", "thing", "give",
+                    "many", "well"]
+
+    for word in ngram:
+        if word in common_words:
+            return True
+
+    return False
+
+
 def clean_input(text):
     """
     Очистка данных
@@ -28,7 +53,10 @@ def ngrams(text, n):
     text = clean_input(text)
     output = {}
     for i in range(len(text) - n + 1):
+        if is_common(text[i:i+n]):
+            continue
         ngram_temp = " ".join(text[i:i + n])
+
         if ngram_temp not in output:
             output[ngram_temp] = 0
         output[ngram_temp] += 1
